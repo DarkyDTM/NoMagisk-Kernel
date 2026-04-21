8ec40a8609fabe7197dbcf972fe4bc1f51dce492
DarkyDTM <thedarkygd@gmail.con>
2026-04-21 17:12:24 +0500

    added anykernel3 to github actions

---
+++ tools/anykernel3/anykernel.sh
@@ -0,0 +1,25 @@
+# AnyKernel3 for OnePlus 9 Pro (lemonadep) - lahaina
+properties() { '
+kernel.string=NoMagisk kernel for OnePlus 9 Pro
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=lemonadep
+device.name2=OnePlus9Pro
+device.name3=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; }
+
+BLOCK=boot;
+IS_SLOT_DEVICE=1;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+. tools/ak3-core.sh;
+
+split_boot;
+flash_boot;
