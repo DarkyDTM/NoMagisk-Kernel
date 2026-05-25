253c21aa4923077de8f6eb00cc70fdef8aa82d2e
DarkyDTM <darkdtm@proton.me>
2026-05-25 13:22:41 +0500

    add hook to fs/open.c

--- fs/open.c
+++ fs/open.c
@@ -35,6 +35,13 @@
 
 #include "internal.h"
 
+// KSUN hook
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+                int *mode, int *flags);
+#endif
+
 int do_truncate(struct dentry *dentry, loff_t length, unsigned int time_attrs,
 	struct file *filp)
 {
@@ -442,6 +449,12 @@ out:
 
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
+
+// KSUN hook
+#ifdef CONFIG_KSU
+    ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
+
 	return do_faccessat(dfd, filename, mode);
 }
 
