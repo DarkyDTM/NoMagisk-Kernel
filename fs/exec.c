8515b3abed1ffbae6f1be2da930b00392e020a51
DarkyDTM <darkdtm@proton.me>
2026-05-26 13:30:40 +0500

    remove manual hooks from fs/exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -73,13 +73,6 @@
 
 #include <trace/events/sched.h>
 
-// KSUN hook
-#ifdef CONFIG_KSU
-__attribute__((hot))
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
-                void *argv, void *envp, int *flags);
-#endif
-
 int suid_dumpable = 0;
 
 static LIST_HEAD(formats);
@@ -1934,11 +1927,6 @@ int do_execve(struct filename *filename,
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 
-// KSUN hook
-#ifdef CONFIG_KSU
-    ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
-#endif
-
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1966,11 +1954,6 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-
-// KSUN hook
-#ifdef CONFIG_KSU // 32-bit ksud and 32-on-64 support
-    ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
-#endif
 
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
